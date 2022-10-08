a90e776c83160272abae5b1a04abf75b5716f82f
aysllanferreira <aysllan.ferreiraks@gmail.com>
2022-10-08 16:50:46 -0300

    Feito ate a parte do desafio

--- src/script.js
+++ src/script.js
@@ -1,1 +1,32 @@
-// Seu código aqui
+const getBtn = document.querySelector('#button');
+const getChecked = document.querySelector('#agreement');
+const getForms = document.querySelector('#forms');
+
+getBtn.disabled = true;
+getBtn.style.opacity = '50%';
+
+function sendInformation () {
+    getBtn.addEventListener('click', function () {
+        
+        // while(getForms.childNodes.length > 0){
+        //     getForms.removeChild(getForms.firstChild);
+        // }
+        getForms.innerHTML = '';
+        getForms.style.textAlign = 'center';
+        getForms.style.fontSize = '30px';
+        getForms.innerText = 'Formulário enviado com sucesso!'
+    })
+}
+
+
+getChecked.addEventListener('click', () => {
+    if(getBtn.disabled === true){
+        getBtn.disabled = false;
+        getBtn.style.opacity = '100%';
+    }else {
+        getBtn.disabled = true;
+        getBtn.style.opacity = '50%';
+    }
+})
+
+sendInformation();
